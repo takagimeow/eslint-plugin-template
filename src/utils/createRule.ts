3d7c03c05c9802131062b4b695f28ea4b52b937c
Keisuke Takagi <mit@takagi.app>
2024-01-05 04:42:03 +0900

    feat: createRule関数を実装

---
+++ src/utils/createRule.ts
@@ -0,0 +1,9 @@
+import { ESLintUtils } from '@typescript-eslint/utils';
+
+// TODO: ドキュメントのURLを用意する
+const createRule = ESLintUtils.RuleCreator(
+  name =>
+    `TODO`,
+);
+
+export { createRule };
