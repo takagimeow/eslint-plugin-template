cd00ee3a81fb45727a9244ea65aa19a53616a8df
Keisuke Takagi <mit@takagi.app>
2024-01-06 02:16:33 +0900

    chore: eslintの設定ファイルの内容を更新
    - @typescript-eslint/recommendedを継承
    - parserに@typescript-eslint/parserを設定
    - 文法的にはES2021をサポートするためecmaVersionに12を設定
    - @typescript-eslintをプラグインに追加
    - 自作のプラグインをローカルで試せるようにするため、eslint-plugin-local-rulesをプラグインに追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
-"use strict";
-
 module.exports = {
   root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
-    },
-  ],
+  plugins: ["@typescript-eslint", "local-rules"],
+  env: {
+    es2019: true,
+  },
+  rules: {
+    "no-undef": ["off"],
+  },
+  ignorePatterns: [".eslintrc.js", "eslint-local-rules.js"],
 };
